e56b83920a9fdcd96f031602aef8884b45b41217
Remix Workspace <>
2022-10-13 16:09:46 -0400

    init

--- contracts/ScoreCard.sol
+++ contracts/ScoreCard.sol
@@ -12,6 +12,16 @@ contract ScoreCard {
         classTeacher = msg.sender;
     }
 
+    /**
+    this modifire allows only class teacher address to acess.
+    this method call would be added to methods which only suppose to be called by 
+    class teacher. In this we added it to both addStudent and addScoreCard. 
+    **/
+    modifier onlyClassTeacher( address _classTeacher){
+        require(classTeacher ==_classTeacher,"Only the class teacher has access to this function");
+        _;
+    }
+
     struct StudentDetails{
         string studentFirstName;
         string studentLastName;
@@ -28,16 +38,28 @@ contract ScoreCard {
     mapping (uint => StudentDetails) students;
     mapping(uint => Score) scores;
 
-    function addStudentDetails(string memory _studentFirstName, string memory _studentLastName) public
+
+
+    event studentAdded(string  _studentFirstName, string  _studentLastName,uint256 _studentId );
+
+    event studentStudentScoresRecorded(uint256 _studentId, uint256 _englishMarks,uint256 _mathsMarks,uint256 _scienceMarks);
+
+
+    function addStudentDetails (string memory _studentFirstName, string memory _studentLastName) public
+    onlyClassTeacher(msg.sender)
     {
         StudentDetails storage studentObject = students[studentCount];
         studentObject.studentFirstName = _studentFirstName;
         studentObject.studentLastName = _studentLastName;
         studentObject.id = studentCount;
+        emit studentAdded(_studentFirstName,_studentLastName,studentCount);
         studentCount++;
     }
 
-    function addStudentScore(uint256 _studentId, uint256 _englishMarks,uint256 _mathsMarks,uint256 _scienceMarks) public {
+    function addStudentScore(uint256 _studentId, uint256 _englishMarks,uint256 _mathsMarks,uint256 _scienceMarks) public 
+        onlyClassTeacher(msg.sender)
+
+    {
 
         Score storage scoreObject  = scores[_studentId];
 
@@ -45,6 +67,7 @@ contract ScoreCard {
         scoreObject.scienceMarks = _scienceMarks;
         scoreObject.mathsMarks = _mathsMarks;
         scoreObject.studentId =_studentId;
+        emit studentStudentScoresRecorded(_studentId,_englishMarks,_mathsMarks,_scienceMarks);
     }
 
 }
